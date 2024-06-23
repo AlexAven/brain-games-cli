cbf1fb9611826d7a3751fd741425062bb62b3226
Alexander Kazakov <gloom31@gmail.com>
2024-06-23 16:00:01 +0300

    calc-game.js refactoring

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,6 +1,5 @@
 import {
   userNameQuerry,
-  randomElementOfArray,
   randomNumber,
   getAnswer,
   wrongAnswer,
@@ -8,45 +7,42 @@ import {
   gameOver,
 } from '../index.js';
 
+const calculatings = (operand1, operator, operand2) => {
+  switch (operator) {
+    case '+':
+      return String(operand1 + operand2);
+    case '-':
+      return String(operand1 - operand2);
+    case '*':
+      return String(operand1 * operand2);
+    default:
+      return console.log('something went wrong...');
+  }
+};
+
+const randomElementOfArray = (array) => array[Math.floor(Math.random() * (array.length - 1))];
+
 const calcGame = () => {
   const userName = userNameQuerry();
+  const operators = ['+', '-', '*'];
   let triesReamin = 3;
 
-  const calculatings = () => {
-    const operators = ['+', '-', '*'];
-
-    console.log('What is the result of the expression?');
-    while (triesReamin > 0) {
-      const randomOperand1 = randomNumber();
-      const randomOperand2 = randomNumber();
-      const randomOperator = randomElementOfArray(operators);
-      let result;
-
-      switch (randomOperator) {
-        case '+':
-          result = String(randomOperand1 + randomOperand2);
-          break;
-        case '-':
-          result = String(randomOperand1 - randomOperand2);
-          break;
-        case '*':
-          result = String(randomOperand1 * randomOperand2);
-          break;
-        default:
-          console.log('something went wrong...');
-      }
-      const userAnswer = getAnswer(`${randomOperand1} ${randomOperator} ${randomOperand2}`);
-      if (result !== userAnswer) {
-        triesReamin = -1;
-        wrongAnswer(userAnswer, result);
-      } else {
-        triesReamin -= 1;
-        rightAnswer();
-      }
+  console.log('What is the result of the expression?');
+  while (triesReamin > 0) {
+    const randomOperand1 = randomNumber();
+    const randomOperand2 = randomNumber();
+    const randomOperator = randomElementOfArray(operators);
+    const result = calculatings(randomOperand1, randomOperator, randomOperand2);
+    const userAnswer = getAnswer(`${randomOperand1} ${randomOperator} ${randomOperand2}`);
+    if (result !== userAnswer) {
+      triesReamin = -1;
+      wrongAnswer(userAnswer, result);
+    } else {
+      triesReamin -= 1;
+      rightAnswer();
     }
-    gameOver(triesReamin, userName);
-  };
-  calculatings();
+  }
+  gameOver(triesReamin, userName);
 };
 
 export default calcGame;
