21ad82955a6a270cbf1c3ca49cb8d07babff161e
Alexander Kazakov <gloom31@gmail.com>
2024-06-23 16:15:38 +0300

    even-game.js refactoring

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -7,31 +7,32 @@ import {
   gameOver,
 } from '../index.js';
 
+const isEvenNumber = (number) => {
+  if (number % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
 const evenGame = () => {
   const userName = userNameQuerry();
+  let triesRemain = 3;
 
-  const isEvenNumber = () => {
-    let triesRemain = 3;
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  while (triesRemain > 0) {
+    const secretNumber = randomNumber();
+    const userAnswer = getAnswer(secretNumber);
+    const result = isEvenNumber(secretNumber);
 
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    while (triesRemain > 0) {
-      const secretNumber = randomNumber();
-      const userAnswer = getAnswer(secretNumber);
-      let isEven;
-
-      secretNumber % 2 === 0 ? isEven = 'yes' : isEven = 'no';
-      if (isEven === userAnswer) {
-        triesRemain -= 1;
-        rightAnswer();
-      } else {
-        triesRemain = -1;
-        wrongAnswer(userAnswer, isEven);
-      }
+    if (result === userAnswer) {
+      triesRemain -= 1;
+      rightAnswer();
+    } else {
+      triesRemain = -1;
+      wrongAnswer(userAnswer, result);
     }
-    gameOver(triesRemain, userName);
-  };
-
-  isEvenNumber();
+  }
+  gameOver(triesRemain, userName);
 };
 
 export default evenGame;
